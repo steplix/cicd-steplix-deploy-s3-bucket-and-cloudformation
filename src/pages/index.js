650dd8c19fbabb3a45da36284338e1696c8d27a2
Evelyn De León <evelyn@steplix.com>
2022-03-22 09:26:24 -0300

    [SW-4] Base project

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div >
+            <Head>
+                <title>Steplix Front End Base</title>
+                <meta name="description" content="Steplix Front End Base" />
+                <meta name="robots" content="noindex" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="flex items-center justify-center h-screen w-full bg-red-100">
+                <h1 className="text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">Steplix Front End Base</h1>
+            </main>
+        </div>
+    );
+}
