36b40a9dc99bd2fdb345bad345c53b526d8c6ead
Leonardo Sainz <lsainz@steplix.com>
2022-03-22 12:30:21 -0300

    [sw-5]add colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./src/**/*.{ts,tsx,js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary => Blue
+        // secundary => yellow
+        // danger => red
+        // lilac => lilac
+        primary: '#2D82FA',
+        secundary: '#FFD232',
+        danger: '#FA503C',
+        lilac:'#180228'
+      }
+    },
   },
   variants: {
     extend: {},
