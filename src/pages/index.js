2b81a222b551e94854269ad30152d54587f08375
Leonardo Sainz <lsainz@steplix.com>
2022-03-26 12:40:22 -0300

    add class container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
-import Masthead from "@/layouts/Masthead";
-import OurCulture from "@/layouts/OurCulture";
+import Masthead from "@/modules/Masthead";
+import OurCulture from "@/modules/OurCulture";
 import AboutUs from "@/modules/AboutUs";
 import WhatWeDo from "@/modules/WhatWeDo";
 import Head from "next/head";
+import HappyClients from "@/modules/HappyClients";
+import ContactIndex from "@/modules/ContactIndex";
 
 export default function Home() {
     return (
@@ -13,18 +15,22 @@ export default function Home() {
                 <meta name="robots" content="noindex" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="h-screen w-full bg-steplix-lilac">
-                <Masthead />
-                <div className="flex items-center justify-center my-36">
-                    <AboutUs />
-                </div>
-                <div className="flex items-center justify-center my-36">
-                    <WhatWeDo />
-                </div>
-                <div className="flex items-center justify-center my-36">
-                    <OurCulture />
-                </div>
-            </main>
+            <Masthead />
+            <div className="container-steplix">
+                <AboutUs />
+            </div>
+            <div className="container-steplix">
+                <WhatWeDo />
+            </div>
+            <div className="container-steplix">
+                <OurCulture />
+            </div>
+            <div className="container-steplix">
+                <HappyClients />
+            </div>
+            <div className="container-steplix">
+                <ContactIndex />
+            </div>
         </div>
     );
 }
