bd00793100da4fc5530836bf386816dc520b8df5
Leonardo Sainz <lsainz@steplix.com>
2022-03-29 12:58:19 -0300

    [sw-5] fix stepcard component

--- src/modules/EverythingStartsWithAnIdea/index.jsx
+++ src/modules/EverythingStartsWithAnIdea/index.jsx
@@ -1,17 +1,27 @@
 import React, { useContext } from "react";
 import { LanguageContext } from "@/common/components/LanguageContext";
+import StepCard from "@/common/components/StepCard";
 const EverythingStartsWithAnIdea = () => {
     const { texts } = useContext(LanguageContext);
 
     return (
-        <div className="py-44 space-y-5 lg:w-[1330px] w-[345px] text-[14px] lg:text-[24px]">
-            <h1 className="text-[24px] lg:text-[54px] font-extrabold">
-                {texts.process.everythingStartsWithAnIdea.title}
-            </h1>
-            <p>{texts.process.everythingStartsWithAnIdea.descriptionOne}</p>
-            <p>{texts.process.everythingStartsWithAnIdea.descriptionTwo}</p>
-            <p>{texts.process.everythingStartsWithAnIdea.descriptionThree}</p>
-            <p>{texts.process.everythingStartsWithAnIdea.descriptionFour}</p>
+        <div className="pt-44 lg:w-[1330px] w-[345px]">
+            <StepCard
+                title={texts.process.everythingStartsWithAnIdea.title}
+                colorTitle="font-bold text-white"
+                textOne={
+                    texts.process.everythingStartsWithAnIdea.descriptionOne
+                }
+                textTwo={
+                    texts.process.everythingStartsWithAnIdea.descriptionTwo
+                }
+                textThree={
+                    texts.process.everythingStartsWithAnIdea.descriptionThree
+                }
+                textFour={
+                    texts.process.everythingStartsWithAnIdea.descriptionFour
+                }
+            />
         </div>
     );
 };
