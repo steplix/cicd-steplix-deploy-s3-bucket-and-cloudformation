e13c69b89e86507f2f3b770628bc476bc1c13aff
Leonardo Sainz <lsainz@steplix.com>
2022-03-30 13:41:21 -0300

    [sw-5] edit contact form component

--- src/common/components/ContactForm/index.jsx
+++ src/common/components/ContactForm/index.jsx
@@ -2,9 +2,30 @@ import React from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import ButtonSend from "@/common/components/ButtonSend";
+import UploadFile from "@/common/components/UploadFile";
 
-const ContactForm = ({ texts }) => {
+const ContactForm = ({ texts, cv }) => {
     const handleSubmit = async (e) => {
+        let data;
+        data = {
+            idTemplate,
+            idChannel: /* email */ 2,
+            metadata: {
+                subject:
+                    idTemplate === 5
+                        ? "Contact | Steplix | Web Site"
+                        : "Postulation | Steplix | Web Site",
+                to: "lsainz@steplix.com",
+                name: e.target.name.value,
+                nemailame: e.target.email.value,
+                company: e.target.company?.value || "",
+                role: e.target.role?.value || "",
+                howCanWeHelpYou: e.target.howCanWeHelpYou.value,
+                lastname: e.target.lastname?.value || "",
+                linkedin: e.target.linkedin?.value || "",
+                uploadCv: e.target.uploadCv?.value || "",
+            },
+        };
         const idTemplate = 5;
         e.preventDefault();
 
@@ -12,22 +33,7 @@ const ContactForm = ({ texts }) => {
             "http://notifications.steplix.com:8000/notifications",
             {
                 type: "POST",
-                data: {
-                    idTemplate,
-                    idChannel: /* email */ 2,
-                    metadata: {
-                        subject:
-                            idTemplate === 5
-                                ? "Contact | Steplix | Web Site"
-                                : "Postulation | Steplix | Web Site",
-                        to: "lsainz@steplix.com",
-                        name: e.target.name.value,
-                        nemailame: e.target.email.value,
-                        company: e.target.company.value,
-                        role: e.target.role.value,
-                        howCanWeHelpYou: e.target.howCanWeHelpYou.value,
-                    },
-                },
+                data: data,
                 cache: false,
                 crossDomain: true,
             }
@@ -62,8 +68,8 @@ const ContactForm = ({ texts }) => {
                             <input
                                 type="text"
                                 maxLength={60}
-                                name="email"
-                                placeholder={texts.email}
+                                name={cv ? "lastname" : "email"}
+                                placeholder={cv ? texts.lastname : texts.email}
                                 className="mb-4 block w-full appearance-none focus:outline-none bg-transparent"
                             />
                         </div>
@@ -74,8 +80,8 @@ const ContactForm = ({ texts }) => {
                             <input
                                 type="text"
                                 maxLength={60}
-                                name="company"
-                                placeholder={texts.company}
+                                name={cv ? "email" : "company?"}
+                                placeholder={cv ? texts.email : texts.company}
                                 className="mb-4 block w-full appearance-none focus:outline-none bg-transparent"
                             />
                         </div>
@@ -83,18 +89,24 @@ const ContactForm = ({ texts }) => {
                             <input
                                 type="text"
                                 maxLength={60}
-                                name="role"
-                                placeholder={texts.role}
+                                name={cv ? "linkedin" : "role"}
+                                placeholder={cv ? texts.linkedin : texts.role}
                                 className="mb-4 block w-full appearance-none focus:outline-none bg-transparent"
                             />
                         </div>
                     </div>
+                    {cv && <UploadFile text={texts.uploadCv} />}
+
                     <div className="relative lg:w-[920px] rounded-md border-2 mx-auto">
                         <textarea
                             name="howCanWeHelpYou"
                             id="text-help-you"
                             maxLength={1000}
-                            placeholder={texts.howCanWeHelpYou}
+                            placeholder={
+                                cv
+                                    ? texts.weWantToKnowAboutYou
+                                    : texts.howCanWeHelpYou
+                            }
                             className="mt-5 ml-3 block w-full appearance-none focus:outline-none bg-transparent h-40"
                         ></textarea>
                         <div className="float-right mt-10">
--- src/modules/ContactUs/index.jsx
+++ src/modules/ContactUs/index.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import { LanguageContext } from "@/common/components/LanguageContext";
 import ContactForm from "@/common/components/ContactForm";
 
-const ContactUs = () => {
+const ContactUs = ({ cv }) => {
     const { texts } = useContext(LanguageContext);
 
-    return <ContactForm texts={texts.contactUs} />;
+    return <ContactForm cv={cv && cv} texts={texts.contactUs} />;
 };
 
 export default ContactUs;
