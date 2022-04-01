375c333f1c917fcc2141973a4b88bb4a420befc1
Leonardo Sainz <lsainz@steplix.com>
2022-04-01 19:22:01 -0300

    [sw-10] fix center modal

--- src/common/components/Modal/index.jsx
+++ src/common/components/Modal/index.jsx
@@ -40,7 +40,7 @@ export default function Modal({
                     <div className="h-full w-full flex justify-center items-center fixed inset-0 z-[99999] outline-none focus:outline-none text-white">
                         <div
                             ref={ref}
-                            className="wrapper bottom-36 relative lg:h-[500px] h-[800px] right-[6%] 2xl:left-[11%] sm:left-[7%] w-[97%] lg:w-[1000px] flex justify-center "
+                            className="wrapper lg:bottom-36 relative lg:h-[500px] h-[800px] right-[6%] 2xl:left-[11%] sm:left-[7%] w-[97%] lg:w-[1000px] flex justify-center "
                         >
                             {/*content*/}
                             <div
