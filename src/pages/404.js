27cc637a6d417df0875a0d66fe7696abb254c3b9
Leonardo Sainz <lsainz@steplix.com>
2022-04-12 12:48:24 -0300

    [sw-26]  add responsive 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,9 +10,34 @@ const Custom404 = () => {
 
     return (
         <div className="relative h-screen">
-            <div className="py-[220px] section-steplix space-x-10 lg:flex items-center">
+            <div className="lg:hidden">
+                <div className="pt-[110px] section-steplix">
+                    <div className="text-center">
+                        <h1 className="text-[64px] font-black mb-8 ">
+                            Error <span className="title-empty"> 404</span>
+                        </h1>
+                        <p className="p-steplix w-[327px] text-left leading-10">
+                            {t("404.subtitle")}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full h-[176px] mt-11">
+                    <img src="/assets/img/404.svg" alt="logo" layout="fill" />
+                </div>
+                <div className="section-steplix mt-[100px] flex justify-center">
+                    <Link href={{ pathname: "/", query: query }}>
+                        <a>
+                            {" "}
+                            <button className="w-[138px] h-[39px] bg-steplix-blue rounded-lg p-steplix">
+                                {t("404.button")}
+                            </button>{" "}
+                        </a>
+                    </Link>
+                </div>
+            </div>
+            <div className="py-[220px] section-steplix space-x-10 lg:flex items-center hidden ">
                 <div>
-                    <div className=" lg:w-[777px] lg:h-[384px] w-[333px] h-[170px]">
+                    <div className=" lg:w-[777px] h-[384px]">
                         <img
                             src="/assets/img/404.svg"
                             alt="logo"
@@ -21,10 +46,10 @@ const Custom404 = () => {
                     </div>
                 </div>
                 <div>
-                    <h1 className="text-[50px] lg:text-[100px] font-black">
+                    <h1 className="text-[100px] font-black">
                         Error <span className="title-empty"> 404</span>
                     </h1>
-                    <p className="p-steplix lg:w-[500px] w-[270px] leading-10">
+                    <p className="p-steplix w-[500px] leading-10">
                         {t("404.subtitle")}
                     </p>
 
