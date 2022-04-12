7ceaa9935adfb2b0990ab9ae9a49e76d5b9e3313
Leonardo Sainz <lsainz@steplix.com>
2022-04-12 14:24:04 -0300

    [sw-26]  fixed responsive 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,54 +9,38 @@ const Custom404 = () => {
     const [query] = useLanguageQuery();
 
     return (
-        <div className="relative h-screen">
-            <div className="lg:hidden">
-                <div className="pt-[110px] section-steplix">
-                    <div className="text-center">
-                        <h1 className="text-[64px] font-black mb-8 ">
-                            Error <span className="title-empty"> 404</span>
+        <div className="relative h-screen w-full">
+            <div className="lg:py-[220px] pt-[110px] flex lg:justify-center flex-col lg:items-center lg:flex-row-reverse">
+                <div className="w-full lg:w-[700px]">
+                    <div className="lg:ml-8">
+                        <h1 className="lg:text-[100px] text-[64px] mb-8 font-black text-center lg:text-left">
+                            Error<span className="title-empty"> 404</span>
                         </h1>
-                        <p className="p-steplix w-[327px] text-left leading-10">
-                            {t("404.subtitle")}
-                        </p>
+                        <div className="mx-auto lg:mx-0 w-[327px] lg:w-[500px]">
+                            <p className="p-steplix lg:leading-10 leading-6">
+                                {t("404.subtitle")}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="hidden lg:block lg:ml-8">
+                        <Link href={{ pathname: "/", query: query }}>
+                            <a>
+                                <button className="lg:w-[194px] lg:h-[53px] lg:mt-12 bg-steplix-blue rounded-lg p-steplix">
+                                    {t("404.button")}
+                                </button>{" "}
+                            </a>
+                        </Link>
                     </div>
                 </div>
-                <div className="w-full h-[176px] mt-11">
+                <div className="lg:w-[777px] lg:h-[384px] w-full h-[176px] mt-11 md:mx-0">
                     <img src="/assets/img/404.svg" alt="logo" layout="fill" />
                 </div>
-                <div className="section-steplix mt-[100px] flex justify-center">
+                <div className="lg:hidden mt-[100px] flex justify-center ">
                     <Link href={{ pathname: "/", query: query }}>
                         <a>
                             {" "}
                             <button className="w-[138px] h-[39px] bg-steplix-blue rounded-lg p-steplix">
-                                {t("404.button")}
-                            </button>{" "}
-                        </a>
-                    </Link>
-                </div>
-            </div>
-            <div className="py-[220px] section-steplix space-x-10 lg:flex items-center hidden ">
-                <div>
-                    <div className=" lg:w-[777px] h-[384px]">
-                        <img
-                            src="/assets/img/404.svg"
-                            alt="logo"
-                            layout="fill"
-                        />
-                    </div>
-                </div>
-                <div>
-                    <h1 className="text-[100px] font-black">
-                        Error <span className="title-empty"> 404</span>
-                    </h1>
-                    <p className="p-steplix w-[500px] leading-10">
-                        {t("404.subtitle")}
-                    </p>
-
-                    <Link href={{ pathname: "/", query: query }}>
-                        <a>
-                            {" "}
-                            <button className="w-[194px] h-[53px] mt-12 bg-steplix-blue rounded-lg p-steplix">
                                 {t("404.button")}
                             </button>{" "}
                         </a>
