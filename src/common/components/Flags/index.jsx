412a7af78347d6a28354f3886e5e0791b41f6c79
Evelyn De León <evelyn@steplix.com>
2022-04-29 11:53:02 -0300

    [SW-55] Add container to navbar responsive

--- src/common/components/Flags/index.jsx
+++ src/common/components/Flags/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Flags = () => {
     return (
         <div className="hidden xl:block">
-            <div className="bg-blue absolute h-full z-40 top-0 bottom-0 w-[30px] right-[90px]" />
-            <div className="bg-red absolute h-full z-40 top-0 bottom-0 w-[30px] right-[50px]" />
-            <div className="bg-yellow absolute h-full z-40 top-0 bottom-0 w-[30px] right-[10px]" />
+            <div className="bg-yellow  absolute h-full z-40 top-0 bottom-0 w-[30px] right-[80px]" />
+            <div className="bg-red absolute h-full z-40 top-0 bottom-0 w-[30px] right-[40px]" />
+            <div className="bg-blue absolute h-full z-40 top-0 bottom-0 w-[30px] right-0" />
         </div>
     );
 };
