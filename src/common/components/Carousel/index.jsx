c41e3699b7bf909401b7f757b9501b5770422e16
Leonardo Sainz <lsainz@steplix.com>
2022-05-05 12:12:19 -0300

    [sw-69] added arrows to carrousel

--- src/common/components/Carousel/index.jsx
+++ src/common/components/Carousel/index.jsx
@@ -177,17 +177,17 @@ const Carousel = () => {
                     </div>
                 </div>
                 {/* show when it is desktop or bigger */}
-                <div className="hidden xl:block text-white">
+                <div className="hidden xl:block text-white lg:mr-10 xl:ml-0">
                     <div className="navigation-wrapper">
                         <div ref={sliderRef} className="keen-slider flex">
                             {_.chunk(ARRAY_CLIENTS, 1).map((subimages, i) => (
                                 <div
                                     key={`subimage-${i}`}
-                                    className="keen-slider__slide number-slide gap-12"
+                                    className="keen-slider__slide number-slide"
                                 >
                                     {subimages.map((element, j) => (
                                         <div
-                                            className={`${element?.borderColor} border-2 rounded-3xl xl:h-[520px] xl:w-[450px] 2xl:h-[580px] 2xl:w-[520px] mb-8`}
+                                            className={`${element?.borderColor} border-2 rounded-3xl xl:h-[520px] lg:w-[400px] xl:w-[450px] 2xl:h-[580px] 2xl:w-[520px] mb-8`}
                                             key={j}
                                         >
                                             <div className="flex justify-end p-7">
@@ -235,6 +235,31 @@ const Carousel = () => {
                                 </div>
                             ))}
                         </div>
+                        {loaded && propsRef.current && (
+                            <>
+                                <Arrow
+                                    left
+                                    onClick={(e) =>
+                                        e.stopPropagation() ||
+                                        propsRef.current?.prev()
+                                    }
+                                    disabled={currentSlide === 0}
+                                />
+
+                                <Arrow
+                                    onClick={(e) =>
+                                        e.stopPropagation() ||
+                                        propsRef.current?.next()
+                                    }
+                                    disabled={
+                                        currentSlide ===
+                                        propsRef.current.track.details.slides
+                                            .length -
+                                            1
+                                    }
+                                />
+                            </>
+                        )}
                     </div>
                 </div>
             </Fade>
@@ -242,4 +267,25 @@ const Carousel = () => {
     );
 };
 
+function Arrow(props) {
+    return (
+        <svg
+            onClick={props.onClick}
+            className={`h-8 w-8 absolute top-[50%] fill-[#fff] ${
+                props.left
+                    ? "left-10 lg:left-16 xl:left-1 2xl:left-1"
+                    : "right-10 lg:right-16 xl:right-1 2xl:right-1"
+            }`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+        >
+            {props.left && (
+                <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+            )}
+            {!props.left && (
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+            )}
+        </svg>
+    );
+}
 export default Carousel;
