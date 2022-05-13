644d6c693874e5777ea9e0c6baa55f4a9831e5a8
Evelyn De León <evelyn@steplix.com>
2022-05-13 09:53:22 -0300

    [SW-61] ImageFeatureCard created and used in applicable cases

--- src/pages/stack/index.js
+++ src/pages/stack/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Flags from "@/common/components/Flags";
 import { useTranslation } from "next-export-i18n";
-import StepCard from "@/common/components/StepCard";
+import ImageFeatureCard from "@/common/components/ImageFeatureCard";
 import Fade from "react-reveal/Fade";
 
 const Stack = () => {
@@ -61,7 +61,7 @@ const Stack = () => {
                 />
             </Head>
             <div className="relative">
-                <div className="bg--stack">
+                <div className="bg--stack flex flex-col space-y-20">
                     <section className="container px-5 md:px-0 xl:pr-24">
                         <Fade cascade>
                             <div className="pt-[128px] mb-8 2xl:mb-12 ">
@@ -77,135 +77,118 @@ const Stack = () => {
                             </div>
                         </Fade>
                         <Fade cascade>
-                            <div className="grid xl:grid-cols-3 lg:flex-row gap-y-5 lg:gap-x-1 2xl:gap-y-10 mb-12 2xl:w-[1200px] ">
+                            <div className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 md:gap-y-12 xl:gap-y-10 xl:gap-x-12">
                                 <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[82px] h-[28px] md:w-[102px] md:h-[34px] lg:w-[136px] lg:h-[46px] xl:w-[108px] xl:h-[37px] 2xl:w-[136px] 2xl:h-[46px]"
-                                        colorTitle="font-semibold text-white"
-                                        textOne={t("stack.cards.react")}
-                                        colorLine="bg-yellow"
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/react.svg"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/react.svg"
+                                        imageAlt="react"
+                                        imageSize="w-[136px] h-[46px] xl:w-[131px] xl:h-[44px] 2xl:w-[136px] 2xl:h-[46px]"
+                                        description={t("stack.cards.react")}
+                                        divideColor="bg-yellow"
                                     />
                                 </div>
                                 <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[58px] h-[23px] md:w-[81px] md:h-[33px] lg:w-[102px] lg:h-[42px] xl:w-[81px] xl:h-[33px] 2xl:w-[102px] 2xl:h-[61px]"
-                                        colorTitle="font-semibold text-white"
-                                        colorLine="bg-red"
-                                        textOne={t("stack.cards.git")}
-                                        icon={"/assets/img/stack/git.svg"}
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/git.svg"
+                                        imageAlt="git"
+                                        imageSize="w-[102px] h-[42px] xl:w-[98px] xl:h-[41px] 2xl:w-[102px] 2xl:h-[61px]"
+                                        description={t("stack.cards.git")}
+                                        divideColor="bg-red"
                                     />
                                 </div>
                                 <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[89px] h-[23px] md:w-[126px] md:h-[33px] lg:w-[174px] lg:h-[44px] xl:w-[144px] xl:h-[36px] 2xl:w-[174px] 2xl:h-[44px]"
-                                        colorTitle="font-semibold text-white"
-                                        colorLine="bg-blue"
-                                        textOne={t("stack.cards.docker")}
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/docker.svg"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/docker.svg"
+                                        imageAlt="docker"
+                                        imageSize="w-[174px] h-[44px] xl:w-[167px] xl:h-[42px] 2xl:w-[174px] 2xl:h-[44px]"
+                                        description={t("stack.cards.docker")}
+                                        divideColor="bg-blue"
                                     />
                                 </div>
                                 <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[67px] h-[41px] md:w-[78px] md:h-[48px] lg:w-[94px] lg:h-[56px] xl:w-[88px] xl:h-[54px] 2xl:w-[94px] 2xl:h-[58px]"
-                                        colorTitle="font-semibold text-white"
-                                        textOne={t("stack.cards.next")}
-                                        colorLine="bg-yellow"
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/next.svg"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/next.svg"
+                                        imageAlt="next"
+                                        imageSize="w-[100px] h-[60px] md:w-[110px] md:h-[66px] xl:w-[98px] xl:h-[59px] 2xl:w-[94px] 2xl:h-[58px]"
+                                        description={t("stack.cards.next")}
+                                        divideColor="bg-yellow"
                                     />
                                 </div>
                                 <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[89px] h-[23px] md:w-[107px] md:h-[31px] lg:w-[146px] lg:h-[42px] xl:w-[132px] xl:h-[39px] 2xl:w-[146px] 2xl:h-[42px]"
-                                        colorTitle="font-semibold text-white"
-                                        textOne={t("stack.cards.flutter")}
-                                        colorLine="bg-red"
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/flutter.svg"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/flutter.svg"
+                                        imageAlt="flutter"
+                                        imageSize="w-[146px] h-[42px] xl:w-[130px] xl:h-[37px] 2xl:w-[146px] 2xl:h-[42px]"
+                                        description={t("stack.cards.flutter")}
+                                        divideColor="bg-red"
                                     />
                                 </div>
                                 <div className="xl:w-[320px] 2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[55px] h-[33px] lg:w-[71px] lg:h-[42px] xl:w-[66px] xl:h-[71px] 2xl:w-[66px] 2xl:h-[42px]"
-                                        colorTitle="font-semibold text-white"
-                                        colorLine="bg-blue"
-                                        textOne={t("stack.cards.aws")}
-                                        icon={"/assets/img/stack/aws.svg"}
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                    />
-                                </div>
-
-                                <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[55px] h-[37px] md:w-[67px] md:h-[41px] w-[45px] h-[67px] xl:w-[39px] xl:h-[59px] 2xl:w-[45px] 2xl:h-[67px]"
-                                        colorTitle="font-semibold text-white"
-                                        textOne={t("stack.cards.figma")}
-                                        colorLine="bg-yellow"
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/figma.svg"}
-                                    />
-                                </div>
-
-                                <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[89px] h-[18px] md:w-[109px] md:h-[24px] lg:w-[153px] lg:h-[34px] xl:w-[144px] xl:h-[32px] 2xl:w-[153px] 2xl:h-[34px]"
-                                        colorTitle="font-semibold text-white"
-                                        colorLine="bg-red"
-                                        textOne={t("stack.cards.kotlin")}
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/kotlin.svg"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/aws.svg"
+                                        imageAlt="aws"
+                                        imageSize="w-[83px] h-[49px] xl:w-[74px] xl:h-[44px] 2xl:w-[66px] 2xl:h-[42px]"
+                                        description={t("stack.cards.aws")}
+                                        divideColor="bg-blue"
                                     />
                                 </div>
                                 <div className="2xl:w-[362px]">
-                                    <StepCard
-                                        widthTexts="md:w-[735px] lg:w-[931px] xl:w-[320px] 2xl:w-[362px]"
-                                        iconStyles="pl-0 w-[67px] h-[41px] md:w-[78px] md:h-[48px] lg:w-[94px] lg:h-[56px] xl:w-[88px] xl:h-[54px] 2xl:w-[94px] 2xl:h-[58px]"
-                                        colorTitle="font-semibold text-white"
-                                        textOne={t("stack.cards.node")}
-                                        colorLine="bg-blue"
-                                        sizeTitle={"text-[22px] xl:text-[50px]"}
-                                        icon={"/assets/img/stack/node.svg"}
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/figma.svg"
+                                        imageAlt="figma"
+                                        imageSize="w-[45px] h-[67px] xl:w-[39px] xl:h-[59px] 2xl:w-[45px] 2xl:h-[67px]"
+                                        description={t("stack.cards.figma")}
+                                        divideColor="bg-yellow"
+                                    />
+                                </div>
+                                <div className="2xl:w-[362px]">
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/kotlin.svg"
+                                        imageAlt="kotlin"
+                                        imageSize="w-[153px] h-[34px] 2xl:w-[153px] 2xl:h-[34px]"
+                                        description={t("stack.cards.kotlin")}
+                                        divideColor="bg-red"
+                                    />
+                                </div>
+                                <div className="2xl:w-[362px]">
+                                    <ImageFeatureCard
+                                        image="/assets/img/stack/node.svg"
+                                        imageAlt="node"
+                                        imageSize="w-[94px] h-[58px] xl:w-[99px] xl:h-[61px] 2xl:w-[94px] 2xl:h-[58px]"
+                                        description={t("stack.cards.node")}
+                                        divideColor="bg-blue"
                                     />
                                 </div>
                             </div>
                         </Fade>
                     </section>
                     <Fade cascade>
-                        <section className="container px-5 xl:px-0 xl:pr-24 pb-[53px] md:pb-[106px] lg:pb-[196px] xl:pb-[142px] 2xl:pb-[150px]">
-                            <h2 className="font-semibold text-4xl md:text-8xl lg:text-9xl xl:text-8xl 2xl:text-9xl w-10 md:w-full mb-6 md:mb-8 lg:mb-10 xl:mb-8 2xl:mb-10">
-                                {t("stack.sectionTwo.title")}
-                                <span className="text--outlined">
-                                    {t("stack.sectionTwo.titleEmpty")}
-                                </span>
-                            </h2>
-                            <p className="paragraph text-center md:w-[735px] lg:w-[931px] xl:w-[1000px] 2xl:w-[1160px]">
-                                HTML • CSS • SASS • Typescript • JQuery •
-                                Angular Js • Angular • ReactJs • NextJs •
-                                Bootstrap • Material • Foundation • Tailwind •
-                                NodeJs • PHP • Java • Python • Ruby • C • C++ •
-                                C# • GO • ASP.NET • Express • Ember • Laravel •
-                                Symfony • CakePHP • Django • Spring • Struts •
-                                Ruby on Rails • MySQL • Mariadb • SQL • Sqlite •
-                                DynamoDB • PostgreSQL • DB2 • HSQLDB • Redis •
-                                MongoDB • ElastiCache • Elasticsearch • Cordova
-                                • Ionic • Flutter • Kotlin • Objective-C • Swift
-                                • Git • SVN • Gitlab • Github • Bitbucket •
-                                Vagrant • Docker • Kubernetes • Linux • Windows
-                                • AWS • Azure • Firebase • WordPress • Ghost •
-                                Drupal
-                            </p>
+                        <section className="container px-5 md:px-0 xl:pr-24 pb-[53px] md:pb-[106px] lg:pb-[196px] xl:pb-[142px] 2xl:pb-[150px]">
+                            <div className="flex flex-col items-center space-y-4">
+                                <h2 className="text-center md:text-left font-semibold text-4xl md:text-8xl lg:text-9xl xl:text-8xl 2xl:text-9xl mb-6 md:mb-8 lg:mb-10 xl:mb-8 2xl:mb-10 w-[190px] md:w-full">
+                                    {t("stack.sectionTwo.title")}
+                                    <span className="text--outlined">
+                                        {t("stack.sectionTwo.titleEmpty")}
+                                    </span>
+                                </h2>
+                                <p className="paragraph text-center md:w-[735px] lg:w-[931px] xl:w-[1000px] 2xl:w-[1160px]">
+                                    HTML • CSS • SASS • Typescript • JQuery •
+                                    Angular Js • Angular • ReactJs • NextJs •
+                                    Bootstrap • Material • Foundation • Tailwind
+                                    • NodeJs • PHP • Java • Python • Ruby • C •
+                                    C++ • C# • GO • ASP.NET • Express • Ember •
+                                    Laravel • Symfony • CakePHP • Django •
+                                    Spring • Struts • Ruby on Rails • MySQL •
+                                    Mariadb • SQL • Sqlite • DynamoDB •
+                                    PostgreSQL • DB2 • HSQLDB • Redis • MongoDB
+                                    • ElastiCache • Elasticsearch • Cordova •
+                                    Ionic • Flutter • Kotlin • Objective-C •
+                                    Swift • Git • SVN • Gitlab • Github •
+                                    Bitbucket • Vagrant • Docker • Kubernetes •
+                                    Linux • Windows • AWS • Azure • Firebase •
+                                    WordPress • Ghost • Drupal
+                                </p>
+                            </div>
                         </section>
                     </Fade>
                 </div>
