aa605cd62137703e001a7b0ef25a3282d6df54d6
IvanMuntian <dmuntian@steplix.com>
2022-12-01 17:05:43 -0300

    [SW-108] Field card values object constant has been created

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -20,3 +20,50 @@ export const cardValues = [
         iconName: 'agility'
     },
 ];
+
+export const fieldCardValues = [
+    {
+        text: 'Agropecuario',
+        iconName: 'agro'
+    },
+    {
+        text: 'Seguros',
+        iconName: 'insurance'
+    },
+    {
+        text: 'Dashboard',
+        iconName: 'dashboard'
+    },
+    {
+        text: 'Farmacéutica',
+        iconName: 'pharmacist'
+    },
+    {
+        text: 'Logística',
+        iconName: 'logistics'
+    },
+    {
+        text: 'Fintech',
+        iconName: 'fintech'
+    },
+    {
+        text: 'Medioambiente',
+        iconName: 'environment'
+    },
+    {
+        text: 'Automotor',
+        iconName: 'car'
+    },
+    {
+        text: 'Comunicaciones',
+        iconName: 'communications'
+    },
+    {
+        text: 'Medio digital',
+        iconName: 'news'
+    },
+    {
+        text: 'Aeronáutica',
+        iconName: 'aeronautics'
+    },
+];
