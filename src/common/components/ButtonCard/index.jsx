4ff9db46db5232428293c649210668ffd42ee00e
Daniel Alanis <dalanis@steplix.com>
2022-12-07 15:06:58 -0300

    Button with fixed to content fit

--- src/common/components/ButtonCard/index.jsx
+++ src/common/components/ButtonCard/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ButtonCard = ({clickEvent, label="Button", buttonImage = null}) => {
   return (
     <button
-      className="flex flex-row justify-center bg-blue active:bg-sky-active text-white w-28 h-7 rounded-xl gap-y-2.5"
+      className="flex flex-row justify-center bg-blue active:bg-sky-active text-white w-fit h-7 rounded-xl gap-y-2.5"
       title=""
       alt={label}
       type="button"
