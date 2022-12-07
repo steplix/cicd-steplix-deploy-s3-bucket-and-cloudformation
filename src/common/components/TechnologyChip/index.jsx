f261c2aa0380d0d18a06873f4b08cdb0291ba502
Daniel Alanis <dalanis@steplix.com>
2022-12-07 15:41:37 -0300

    Modifying inline styles

--- src/common/components/TechnologyChip/index.jsx
+++ src/common/components/TechnologyChip/index.jsx
@@ -6,13 +6,7 @@ const Chip = ({ label = "Front End", image = tagImage }) => {
   return (
     <div className="w-[120px] h-[30px] text-purple-light border border-purple-light rounded-3xl">
       <div className="flex flex-row w-[112px] h-[30px] items-center ">
-        <div
-          style={{
-            marginLeft: "10px",
-            paddingRight: "8px",
-            marginTop: "5px"
-          }}
-        >
+        <div className="ml-[12px] mr-[8px] mt-[5px]">
           <Image alt="Front End Logo" height={20} src={image} width={15} />
         </div>
         <div className="mb-1">{label}</div>
