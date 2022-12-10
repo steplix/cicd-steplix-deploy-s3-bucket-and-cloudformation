d31ce525019e65589e1792c22b4c6d77bc7b4157
Daniel Alanis <dalanis@steplix.com>
2022-12-10 19:24:37 -0300

    Change next/Image for img tag - fixed broken styles - adding of image optional

--- src/common/components/TechnologyChip/index.jsx
+++ src/common/components/TechnologyChip/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import tagImage from "@/public/assets/img/tag-image.png";
+import tagImage from "@/public/static/tag-image.png";
 
-const Chip = ({ label = "Front End", image = tagImage }) => {
+const Chip = ({ label = "Front End", image = null, alt, customClass = "" }) => {
   return (
     <div className="w-[120px] h-[30px] text-purple-light border border-purple-light rounded-3xl">
-      <div className="flex flex-row w-[112px] h-[30px] items-center ">
-        <div className="ml-[12px] mr-[8px] mt-[5px]">
-          <Image alt="Front End Logo" height={20} src={image} width={15} />
-        </div>
-        <div className="mb-1">{label}</div>
+      <div className="flex flex-row justify-center w-fit items-center">
+        {image ? (
+          <img
+            alt={alt}
+            height="20"
+            src={image}
+            width="15"
+            className={customClass}
+          />
+        ) : null}
+        <label className={image ? "ml-2" : ""}>{label}</label>
       </div>
     </div>
   );
