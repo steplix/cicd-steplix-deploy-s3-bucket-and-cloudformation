ff362b5d5e43d346a88f7fb0bcef5c2b3af2fc4b
Daniel Alanis <dalanis@steplix.com>
2022-12-12 15:19:42 -0300

    Changing img for Icon component - adding proptypes

--- src/common/components/ButtonCard/index.jsx
+++ src/common/components/ButtonCard/index.jsx
@@ -1,27 +1,37 @@
-import React from "react";
+import propTypes from "prop-types";
+import Icon from "../Icon";
 
 const ButtonCard = ({
-  clickEvent,
-  label = "Button",
-  buttonImage = null,
+  label,
   title = label,
+  alt = label,
+  clickEvent,
+  iconName,
+  customImageClass,
 }) => {
   return (
     <button
       className="w-fit h-fit bg-blue hover:cursor-pointer active:bg-sky-active text-white rounded-xl gap-y-2.5"
       title={title}
-      alt={label}
+      alt={alt}
       type="button"
       onClick={clickEvent}
     >
-      <div className="flex flex-row items-center mx-3 my-1">
-        {buttonImage ? (
-          <img src={buttonImage} width="15" height="30" alt="Button Image" />
-        ) : null}
-        <label className={buttonImage ? "ml-2" : null}>{label}</label>
+      <div className="flex flex-row items-center mx-3 my-1  ">
+        {iconName && <Icon name={iconName} className={customImageClass} />}
+        <label className={iconName && "ml-2"}>{label}</label>
       </div>
     </button>
   );
 };
 
 export default ButtonCard;
+
+ButtonCard.propTypes = {
+  label: propTypes.string.isRequired,
+  title: propTypes.string,
+  alt: propTypes.string,
+  clickEvent: propTypes.func,
+  iconName: propTypes.string,
+  customImageClass: propTypes.string,
+};
