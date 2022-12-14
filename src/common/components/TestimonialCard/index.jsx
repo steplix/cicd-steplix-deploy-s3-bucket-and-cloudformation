feb659d96f0c3ed28cd5862ac9e84dc5643ab879
Sebastian <slares@steplix.com>
2022-12-14 16:45:27 -0300

    [SW-135] Add background to image

--- src/common/components/TestimonialCard/index.jsx
+++ src/common/components/TestimonialCard/index.jsx
@@ -7,7 +7,7 @@ const Card = ({ clientImageUrl, clientFirstName, clientLastName, clientCompany, 
     <article className="h-[365px] w-[280px] md:w-[488px] md:h-[268px] lg:h-[316px] lg:w-[376px] xl:h-[268px] xl:w-[488px] border p-[23px] rounded-[20px] flex flex-col">
       <div className="flex flex-col justify-between pb-6">
         <div className="flex">
-          <img alt={imageAlt} className="w-[52px] h-[52px] rounded-full border" src={clientImageUrl} />
+          <img alt={imageAlt} className="w-[52px] h-[52px] rounded-full border bg-black" src={clientImageUrl} />
           <div className="pl-[18px] pb-[6px]">
             <h3 className="text-[26px] leading-none font-bold">{clientFirstName}</h3>
             <h3 className="text-[26px] leading-none font-bold">{clientLastName}</h3>
