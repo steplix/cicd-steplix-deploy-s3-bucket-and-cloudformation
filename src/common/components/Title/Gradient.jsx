0b4edb433aaac24a0cc01e91924fa5a665c5d514
Sebastian <slares@steplix.com>
2022-12-15 17:16:01 -0300

    [SW-125] Change title bar color

--- src/common/components/Title/Gradient.jsx
+++ src/common/components/Title/Gradient.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Title = ({ content, secondContent, size, borderPosition = 'bottom', borderWidth, height = '50px', leading = 'leading-10', color = 'title-gradient' }) => {
-  return borderPosition === 'bottom' ? (
+const Title = ({
+  content,
+  secondContent,
+  size,
+  borderPosition = "bottom",
+  borderWidth,
+  height = "50px",
+  leading = "leading-10",
+  color = "title-gradient",
+}) => {
+  return borderPosition === "bottom" ? (
     <>
       <div className="title-gradient pb-[8px]">
         <h2 className={`${size} ${leading} font-bold`}>{content}</h2>
-        {secondContent && <h2 className={`${size} ${leading} font-bold`}>{secondContent}</h2>}
+        {secondContent && (
+          <h2 className={`${size} ${leading} font-bold`}>{secondContent}</h2>
+        )}
       </div>
-      <div className={`${borderWidth} h-[2px] bg-[#00A7E1]`} />
+      <div className={`${borderWidth} h-[2px] bg-blue`} />
     </>
   ) : (
     <div className={`flex ${height} items-center`}>
-      <div className="w-[3px] h-full bg-[#00A7E1]" />
+      <div className="w-[3px] h-full bg-blue" />
       <div className={`${color} pl-4`}>
         <h2 className={`${size} ${leading} font-bold`}>{content}</h2>
-        {secondContent && <h2 className={`${size} ${leading} font-bold`}>{secondContent}</h2>}
+        {secondContent && (
+          <h2 className={`${size} ${leading} font-bold`}>{secondContent}</h2>
+        )}
       </div>
     </div>
   );
@@ -29,7 +42,7 @@ Title.propTypes = {
   secondContent: PropTypes.string,
   size: PropTypes.string,
   height: PropTypes.string,
-  borderPosition: PropTypes.oneOf(['bottom', 'left']),
+  borderPosition: PropTypes.oneOf(["bottom", "left"]),
   borderWidth: PropTypes.string.isRequired,
   leading: PropTypes.string,
   color: PropTypes.string,
