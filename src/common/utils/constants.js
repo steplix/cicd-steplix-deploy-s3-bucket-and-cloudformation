dc32fbdf3855238fd8b37fe0940fa4e4ac7c9f48
IvanMuntian <dmuntian@steplix.com>
2022-12-15 19:16:13 -0300

    [SW-139] Removed unused constant

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -1,26 +1,3 @@
-export const cardValues = [
-    {
-        text: 'Confianza',
-        iconName: 'trust'
-    },
-    {
-        text: 'Respeto mutuo',
-        iconName: 'mutualRespect'
-    },
-    {
-        text: 'Éxito',
-        iconName: 'success'
-    },
-    {
-        text: 'Conciencia',
-        iconName: 'awareness'
-    },
-    {
-        text: 'Agilidad',
-        iconName: 'agility'
-    },
-];
-
 export const fieldCardValues = [
     {
         text: 'Agropecuario',
