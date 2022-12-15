e3c5f0e2b118ff0a9fe172c31521615e4e102943
Sebastian <slares@steplix.com>
2022-12-15 12:59:19 -0300

    [SW-125] Add example titles to show

--- src/common/components/Carousel/index.jsx
+++ src/common/components/Carousel/index.jsx
@@ -6,6 +6,10 @@ import { useKeenSlider } from "keen-slider/react";
 import { useTranslation } from "next-export-i18n";
 import Fade from "react-reveal/Fade";
 import TestimonialCard from "@/common/components/TestimonialCard";
+import Title from '../Title/Gradient';
+import InvertedTitle from '../Title/Inverted';
+
+
 
 const ResizePlugin = (slider) => {
     const observer = new ResizeObserver(function () {
@@ -162,6 +166,53 @@ const Carousel = () => {
 
     return (
         <>
+
+            <Title 
+            content={'Pasos hacia'} 
+            secondContent={'la cima'} 
+            size="text-[32px]" 
+            height="h-[80px]" 
+            borderWidth="w-[101px]" 
+            borderPosition="left" 
+            />
+            <br />
+            <Title 
+            content={'Equipos'} 
+            size="text-[32px]" 
+            height="h-[50px]" 
+            borderPosition="left" 
+            borderWidth="w-[101px]" 
+            color='title-gradient'
+            />
+            <br />
+            <Title 
+            content={'Exploramos'} 
+            secondContent={'tu problema'}
+            leading='leading-6'
+            size="text-[26px]" 
+            borderWidth="w-[101px]" 
+            color='title-gradient'
+            />
+            <br />
+            <Title 
+            content={'Misión'} 
+            leading='leading-6'
+            size="text-[20px]" 
+            height="h-[80px]" 
+            borderWidth="w-[60px]" 
+            color='text-purpleV3'
+            />
+            <br />
+
+            <div className='bg-black p-6'>
+            <InvertedTitle 
+            content={'Nuestro'} 
+            secondContent={'portfolio'} 
+            size="text-[32px]" 
+            leading='leading-[48px]'
+            />
+            </div>
+            <br />
             <Fade cascade>
                 {/* show when it is mobile and tablet */}
                 <div className="block xl:hidden">
