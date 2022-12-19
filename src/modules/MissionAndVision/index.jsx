a7d2241e1a37d2d53d3f2e9e76437436f7ff934e
IvanMuntian <dmuntian@steplix.com>
2022-12-19 16:01:34 -0300

    [SW-144] The title component has been modified

--- src/modules/MissionAndVision/index.jsx
+++ src/modules/MissionAndVision/index.jsx
@@ -9,13 +9,13 @@ const MissionAndVision = () => {
   return (
     <section className="w-full flex flex-col lg:flex-row lg:space-x-8 items-center space-y-8 lg:space-y-0">
         <div className="flex flex-col space-y-4">
-            <h4 className="underlined-title">{t("about_us.mission.title")}</h4>
+            <Title content={t("about_us.mission.title")} />
             <p className="font-poppins text-sm lg:text-base font-normal text-black">
               {t("about_us.mission.description")}  
             </p>
         </div>
         <div className="flex flex-col space-y-4">
-            <h4 className="underlined-title">{t("about_us.vision.title")}</h4>
+            <Title content={t("about_us.vision.title")} />
             <p className="font-poppins text-sm lg:text-base font-normal text-black">
               {t("about_us.vision.description")}  
             </p>
