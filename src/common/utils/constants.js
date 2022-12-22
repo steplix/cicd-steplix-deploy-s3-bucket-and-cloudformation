32b8173c6aa8aeeddcb024b62b27d9b955bdef4e
Matias Ferraro <mferraro@steplix.com>
2022-12-22 02:04:03 +0100

    [FIX] SW-147 fix copy function, fix layout, add avatar photos to cards

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -1,142 +1,145 @@
 export const fieldCardValues = [
-    {
-        text: 'Agropecuario',
-        iconName: 'agro'
-    },
-    {
-        text: 'Seguros',
-        iconName: 'insurance'
-    },
-    {
-        text: 'Dashboard',
-        iconName: 'dashboard'
-    },
-    {
-        text: 'Farmacéutica',
-        iconName: 'pharmacist'
-    },
-    {
-        text: 'Logística',
-        iconName: 'logistics'
-    },
-    {
-        text: 'Fintech',
-        iconName: 'fintech'
-    },
-    {
-        text: 'Medioambiente',
-        iconName: 'environment'
-    },
-    {
-        text: 'Automotor',
-        iconName: 'car'
-    },
-    {
-        text: 'Comunicaciones',
-        iconName: 'communications'
-    },
-    {
-        text: 'Medio digital',
-        iconName: 'news'
-    },
-    {
-        text: 'Aeronáutica',
-        iconName: 'aeronautics'
-    },
+  {
+    text: "Agropecuario",
+    iconName: "agro",
+  },
+  {
+    text: "Seguros",
+    iconName: "insurance",
+  },
+  {
+    text: "Dashboard",
+    iconName: "dashboard",
+  },
+  {
+    text: "Farmacéutica",
+    iconName: "pharmacist",
+  },
+  {
+    text: "Logística",
+    iconName: "logistics",
+  },
+  {
+    text: "Fintech",
+    iconName: "fintech",
+  },
+  {
+    text: "Medioambiente",
+    iconName: "environment",
+  },
+  {
+    text: "Automotor",
+    iconName: "car",
+  },
+  {
+    text: "Comunicaciones",
+    iconName: "communications",
+  },
+  {
+    text: "Medio digital",
+    iconName: "news",
+  },
+  {
+    text: "Aeronáutica",
+    iconName: "aeronautics",
+  },
 ];
 
 export const routesMap = {
-    "/": {
-        title: ""
-    },
-    "/about-us": {
-        title: "navbar.item0"
-    },
-    "/stack": {
-        title: "navbar.item1"
-    },
-    "/process": {
-        title: "navbar.item2"
-    },
-    "/jobs": {
-        title: "navbar.item3"
-    }
-}
+  "/": {
+    title: "",
+  },
+  "/about-us": {
+    title: "navbar.item0",
+  },
+  "/stack": {
+    title: "navbar.item1",
+  },
+  "/process": {
+    title: "navbar.item2",
+  },
+  "/jobs": {
+    title: "navbar.item3",
+  },
+};
 
 export const aboutUsPhotoArray = [
-    {
-        src: "/assets/img/culture/culture-02.png"
-    },
-    {
-        src: "/assets/img/culture/culture-03.png"
-    },
-    {
-        src: "/assets/img/culture/culture-02.png"
-    },
-    {
-        src: "/assets/img/culture/culture-03.png"
-    },
-    {
-        src: "/assets/img/culture/culture-02.png"
-    },
-    {
-        src: "/assets/img/culture/culture-03.png"
-    },
-]
+  {
+    src: "/assets/img/culture/culture-02.png",
+  },
+  {
+    src: "/assets/img/culture/culture-03.png",
+  },
+  {
+    src: "/assets/img/culture/culture-02.png",
+  },
+  {
+    src: "/assets/img/culture/culture-03.png",
+  },
+  {
+    src: "/assets/img/culture/culture-02.png",
+  },
+  {
+    src: "/assets/img/culture/culture-03.png",
+  },
+];
 
 // Constants for Contact page
 
 export const PHOTO_CAROUSEL = [
-    {
-      src: "assets/img/contact/contact-1.png",
-    },
-    {
-      src: "assets/img/contact/contact-2.png",
-    },
-    {
-      src: "assets/img/contact/contact-3.png",
-    },
-    {
-      src: "assets/img/contact/contact-4.png",
-    },
-    {
-      src: "assets/img/contact/contact-5.png",
-    },
-    {
-      src: "assets/img/contact/contact-6.png",
-    },
-  ];
-  
-  export const CARD_CONTACT = [
-    {
-      name: "Kevin",
-      surname: "Halbinger",
-      email: "khalbinger@steplix.com",
-      meeting: "https://meetings.hubspot.com/kevin-halbinger?embed=true%22",
-      linkedin: "https://www.linkedin.com/in/kevin-halbingercofoundersteplix/",
-    },
-  
-    {
-      name: "Enrique",
-      surname: "Serrani",
-      email: "eserrani@steplix.com",
-      meeting: "https://meetings.hubspot.com/enrique-serrani?embed=true",
-      linkedin: "https://www.linkedin.com/in/enrique-serrani-a69a2036/"
-    },
-  
-    {
-      name: "Jorge",
-      surname: "Yánez",
-      email: "jyanez@steplix.com",
-      meeting: "https://meetings.hubspot.com/jyanez1?embed=true",
-      linkedin: "https://www.linkedin.com/in/jorgeyanezy/"
-    },
-  
-    {
-      name: "Mariano",
-      surname: "Wegier",
-      email: "mwegier@steplix.com ",
-      meeting: "https://meetings.hubspot.com/mariano-wegier?embed=true",
-      linkedin: "https://www.linkedin.com/in/marianowegier/"
-    }
-  ]
+  {
+    src: "assets/img/contact/contact-1.png",
+  },
+  {
+    src: "assets/img/contact/contact-2.png",
+  },
+  {
+    src: "assets/img/contact/contact-3.png",
+  },
+  {
+    src: "assets/img/contact/contact-4.png",
+  },
+  {
+    src: "assets/img/contact/contact-5.png",
+  },
+  {
+    src: "assets/img/contact/contact-6.png",
+  },
+];
+
+export const CARD_CONTACT = [
+  {
+    name: "Kevin",
+    surname: "Halbinger",
+    email: "khalbinger@steplix.com",
+    meeting: "https://meetings.hubspot.com/kevin-halbinger?embed=true%22",
+    linkedin: "https://www.linkedin.com/in/kevin-halbingercofoundersteplix/",
+    imgSrc: "assets/img/contact/sellers/seller-1_cropped.png",
+  },
+
+  {
+    name: "Enrique",
+    surname: "Serrani",
+    email: "eserrani@steplix.com",
+    meeting: "https://meetings.hubspot.com/enrique-serrani?embed=true",
+    linkedin: "https://www.linkedin.com/in/enrique-serrani-a69a2036/",
+    imgSrc: "assets/img/contact/sellers/seller-2_cropped.png",
+  },
+
+  {
+    name: "Jorge",
+    surname: "Yánez",
+    email: "jyanez@steplix.com",
+    meeting: "https://meetings.hubspot.com/jyanez1?embed=true",
+    linkedin: "https://www.linkedin.com/in/jorgeyanezy/",
+  },
+
+  {
+    name: "Mariano",
+    surname: "Wegier",
+    email: "mwegier@steplix.com",
+    meeting: "https://meetings.hubspot.com/mariano-wegier?embed=true",
+    linkedin: "https://www.linkedin.com/in/marianowegier/",
+    imgSrc: "assets/img/contact/sellers/seller-4_cropped.png",
+  },
+];
