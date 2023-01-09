8a5bcaa581d4f8908a18043cc9efda341b74ba8d
Matias Ferraro <mferraro@steplix.com>
2023-01-09 19:48:43 +0100

    [SW-178] 2. & 3. add about us icons, change title

--- src/modules/MissionAndVision/index.jsx
+++ src/modules/MissionAndVision/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useTranslation } from "next-export-i18n";
 import Title from "@/common/components/Title/Gradient";
 import Icon from "@/common/components/Icon";
+import VisionIcon from "@/common/components/Icon/icons/VisionIcon";
+import MisionIcon from "@/common/components/Icon/icons/MisionIcon";
 
 const MissionAndVision = () => {
   const { t } = useTranslation();
@@ -9,15 +11,19 @@ const MissionAndVision = () => {
   return (
     <section className="w-full flex flex-col lg:flex-row lg:space-x-8 items-center space-y-8 lg:space-y-0">
       <div className="flex flex-col space-y-4">
-        <Title content={t("about_us.mission.title")} size="text-[26px]" />
+        <div className="flex gap-4 items-center">
+          <Title content={t("about_us.mission.title")} size="text-[26px]" />
+          <MisionIcon />
+        </div>
         <p className="font-poppins text-sm lg:text-base font-normal text-black">
           {t("about_us.mission.description")}
         </p>
       </div>
       <div className="flex flex-col space-y-4">
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
           <Title content={t("about_us.vision.title")} size="text-[26px]" />
-          <Icon name="mission" className="w-6 h-6" />
+          {/* <Icon name="vision" className="w-6 h-6" /> */}
+          <VisionIcon />
         </div>
         <p className="font-poppins text-sm lg:text-base font-normal text-black">
           {t("about_us.vision.description")}
