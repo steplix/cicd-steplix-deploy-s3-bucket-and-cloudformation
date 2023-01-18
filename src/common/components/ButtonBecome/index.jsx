e4eef920f8210aa0b051f1810f855d64d461da93
IvanMuntian <dmuntian@steplix.com>
2023-01-18 14:17:06 -0300

    [SW-202] BecomeStexplierButton component styles has been adjusted

--- src/common/components/ButtonBecome/index.jsx
+++ src/common/components/ButtonBecome/index.jsx
@@ -7,7 +7,7 @@ import { LOCALE_SLUGS } from "@/common/utils/constants";
 const ButtonBecomeSteplixer = ({ locale, customStyles }) => {
   const { t } = useTranslation(locale);
   return (
-    <div className={`text-center text-blue font-bold h font-poppins flex gap-4 items-center justify-center lg:bg-blue lg:text-white lg:rounded-3xl lg:justify-center ${customStyles}`}>
+    <div className={`text-center text-blue font-bold w-fit pl-3 pr-2 font-poppins flex gap-2 items-center justify-center lg:bg-blue lg:text-white lg:rounded-3xl lg:justify-center ${customStyles}`}>
       <CustomNextLink to={`/${locale}/become-steplixer`}>{t("ButtonBecomeSteplixer")}</CustomNextLink>
       <Icon name="chevronRight" className="w-4" fill="currentColor" />
     </div>
