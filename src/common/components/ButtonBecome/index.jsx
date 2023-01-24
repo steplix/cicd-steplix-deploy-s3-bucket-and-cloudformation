bcb6ef55181b2cb6d3ee4e725a0984246ddb328f
IvanMuntian <dmuntian@steplix.com>
2023-01-24 15:14:42 -0300

    [SW-218] Become Steplixer button on hover animation has been implemented

--- src/common/components/ButtonBecome/index.jsx
+++ src/common/components/ButtonBecome/index.jsx
@@ -1,4 +1,5 @@
 import CustomNextLink from "@/common/components/CustomNextLink";
+import { motion } from "framer-motion"; 
 import PropTypes from 'prop-types';
 import Icon from "@/components/Icon";
 import { useTranslation } from "@/common/lib/i18n";
@@ -7,10 +8,12 @@ import { LOCALE_SLUGS } from "@/common/utils/constants";
 const ButtonBecomeSteplixer = ({ locale, customStyles }) => {
   const { t } = useTranslation(locale);
   return (
-    <div className={`text-center text-blue font-bold h font-poppins flex gap-4 items-center justify-center lg:bg-blue lg:text-white lg:rounded-3xl lg:justify-center ${customStyles}`}>
-      <CustomNextLink to={`/${locale}/become-steplixer`}>{t("ButtonBecomeSteplixer")}</CustomNextLink>
-      <Icon name="chevronRight" className="w-4" fill="currentColor" />
-    </div>
+    <CustomNextLink to={`/${locale}/become-steplixer`}>
+      <motion.button className={`relative text-center text-blue font-bold w-fit pl-3 pr-2 font-poppins flex gap-2 items-center justify-center lg:bg-blue lg:text-white lg:rounded-3xl lg:justify-center ${customStyles} btn btn--raise`} whileHover={{ translateY: '-0.25em', transition: '0.10s' }}>
+        {t("ButtonBecomeSteplixer")}
+        <Icon name="chevronRight" className="w-4" fill="currentColor" />
+      </motion.button>
+    </CustomNextLink>
   );
 };
 
