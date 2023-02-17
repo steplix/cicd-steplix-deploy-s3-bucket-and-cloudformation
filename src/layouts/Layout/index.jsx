40583437f72d49b987278e9bc56ec55768be05a6
IvanMuntian <dmuntian@steplix.com>
2023-02-17 17:19:51 -0300

    [SW-257] QA Review changes has been implemented

--- src/layouts/Layout/index.jsx
+++ src/layouts/Layout/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useRouter} from "next/router";
 import PropTypes from "prop-types";
 import Navbar from "@/layouts/Navbar";
 import Footer from "@/layouts/Footer";
 
 const Layout = ({ children }) => {
+
+    const { route } = useRouter();
+
     return (
         <>
             <Navbar />
-            <div className="mt-[56px] sm:mt-[62px] lg:mt-0">
+            <div className={`${route === '/' ? 'mt-[56px] sm:mt-[62px] ' : 'mt-[130px]'} lg:mt-0`}>
                 {children}
             </div>
             <Footer />
