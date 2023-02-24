36cf9542c156b57a2751ba836621205aede2780a
IvanMuntian <dmuntian@steplix.com>
2023-02-24 11:50:49 -0300

    [SW-252] Testimonial card changes has been applied

--- src/common/components/TestimonialCard/index.jsx
+++ src/common/components/TestimonialCard/index.jsx
@@ -16,15 +16,15 @@ const Card = ({
 
   return (
     // Todo remover bordes de imagen y card, una vez que se defina el fondo / imagenes
-    <article className="h-[365px] w-[280px] md:w-[488px] md:h-[268px] lg:h-[316px] lg:w-[376px] xl:h-[268px] xl:w-[488px] p-[23px] rounded-[20px] flex flex-col bg-white card-shadow">
+    <article className="h-[365px] w-[280px] md:w-[488px] md:h-[268px] lg:h-[316px] lg:w-[376px] xl:h-[268px] xl:w-[488px] p-[23px] rounded-[20px] flex flex-col bg-white card-shadow text-black">
       <div className="flex flex-col justify-between pb-6">
-        <div className="flex">
+        <div className="flex space-x-3 mb-[10px]">
           <img
             alt={imageAlt}
-            className="w-[52px] h-[52px] rounded-full bg-black"
+            className="w-[56px] h-[56px] rounded-full"
             src={clientImageUrl}
           />
-          <div className="pl-[18px] pb-[6px]">
+          <div className="text-left">
             <h3 className="text-[26px] leading-none font-bold">
               {clientFirstName}
             </h3>
