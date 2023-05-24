d1ff150446a4912a31d748d2accef561d0e13b46
Matias Duarte <matias@steplix.com>
2023-05-24 19:48:17 -0300

    Added previous version of the website routes to give retro-compatibility with the already generated SEO of those routes

---
+++ src/pages/jobs/index.jsx
@@ -0,0 +1,13 @@
+import BecomeSteplixer from "../[locale]/become-steplixer";
+
+export default function Jobs() {
+  return BecomeSteplixer("en");
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      ...params,
+    },
+  };
+}
---
+++ src/pages/process/index.jsx
@@ -0,0 +1,13 @@
+import WhatWeDo from "../[locale]/what-we-do";
+
+export default function Process() {
+  return WhatWeDo("en");
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      ...params,
+    },
+  };
+}
