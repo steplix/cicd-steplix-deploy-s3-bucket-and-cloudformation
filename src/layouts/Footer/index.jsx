cc2579477c4b612bcd2d5860f99dd24e45cfd054
Evelyn De León <evelyn@steplix.com>
2023-06-14 12:13:25 -0300

    [SW-296] Updated footer styles and IRAN logo

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -13,18 +13,18 @@ const Footer = () => {
   const { t } = useTranslation(i18nQuery?.locale);
 
   return (
-    <footer className="footer-gradient h-[518.62px] pt-8 pb-[72px] sm:pt-[25px] sm:pb-[65px] lg:pt-6 lg:pb-8 sm:h-[271px] lg:h-[276.5px]">
-      <div className="container flex flex-col gap-y-[4.5rem] sm:gap-y-4">
-        <div className="flex flex-col items-center sm:flex-row lg:flex-col sm:items-start lg:space-y-4">
-          <div className="flex flex-col items-center sm:items-start w-full">
+    <footer className="footer-gradient h-[519px] pt-8 pb-[72px] sm:pt-6 sm:pb-8 sm:h-[235px] lg:h-[207px]">
+      <div className="container sm:flex sm:justify-between">
+        <div className="flex flex-col gap-y-12 sm:gap-y-4">
+          <div className="flex flex-col gap-8 sm:gap-4 items-center sm:items-start w-full">
             <div>
               <img
                 src="/assets/img/logo-navbar.svg"
                 alt="Steplix logo"
-                className="w-[70px] h-[18px] lg:w-[86px] lg:h-[22px]"
+                className="w-[70px] h-[18px] sm:w-[86px] sm:h-[22px]"
               />
             </div>
-            <div className="mt-6 lg:mt-4 text-white text-xs leading-6 tracking-[0.03em] lg:text-base lg:leading-8 font-medium flex flex-wrap justify-center sm:justify-start gap-1 max-w-[187px] lg:w-full lg:max-w-none lg:justify-center">
+            <div className="text-white text-xs leading-6 tracking-[0.03em] sm:text-sm sm:leading-7 lg:text-base lg:leading-8 font-medium flex flex-wrap justify-center sm:justify-start gap-1 max-w-[187px] lg:w-full lg:max-w-none lg:justify-center">
               <CustomNextLink to={`/${i18nQuery?.lang}/about-us`}>
                 <a aria-label={t("navbar.item1.ariaLabel")}>{t("footer.links.item0")} </a>
               </CustomNextLink>
@@ -48,16 +48,14 @@ const Footer = () => {
               </CustomNextLink>
             </div>
           </div>
-          <div className="mt-8 lg:mt-4 sm:mt-0 sm:mb-7 flex flex-col lg:flex-row lg:space-x-4 self-center">
-            <div className="flex space-x-4 self-center sm:self-end sm:mb-1 lg:mb-0">
-              <a className="rounded-full h-8 w-8 flex items-center justify-center bg-icon" href="https://www.instagram.com/steplixsoftware/" rel="noreferrer" target="_blank" aria-label={t("footer.instagramAriaLabel")}>
-                <Icon name="instagram" className="h-4 w-4 text-white" />
-              </a>
-              <a className="rounded-full h-8 w-8 flex items-center justify-center bg-icon" href="https://www.linkedin.com/company/steplix" rel="noreferrer" target="_blank" aria-label={t("footer.linkedInAriaLabel")}>                
-                <Icon name="linkedIn" className="h-4 w-4 text-white" />   
-              </a>
-            </div>
-            <div className="flex space-x-2 font-poppins text-white items-center text-mobile mt-3 lg:mt-0">
+          <div className="flex justify-center sm:justify-start gap-3 sm:gap-4">
+            <a className="rounded-full h-8 w-8 flex items-center justify-center bg-icon" href="https://www.instagram.com/steplixsoftware/" rel="noreferrer" target="_blank" aria-label={t("footer.instagramAriaLabel")}>
+              <Icon name="instagram" className="h-4 w-4 text-white" />
+            </a>
+            <a className="rounded-full h-8 w-8 flex items-center justify-center bg-icon" href="https://www.linkedin.com/company/steplix" rel="noreferrer" target="_blank" aria-label={t("footer.linkedInAriaLabel")}>                
+              <Icon name="linkedIn" className="h-4 w-4 text-white" />   
+            </a>
+            <div className="flex gap-[5px] font-poppins text-white items-center text-mobile">
               <Icon
                 name="mail"
                 className="h-[19px] w-[19px] text-white"
@@ -68,38 +66,49 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col-reverse items-center sm:flex-row sm:items-end lg:items-end sm:justify-between text-white text-xs">
-        <div className="flex flex-col gap-2 mt-11 sm:mt-0">
-          <p className="text-[10px] leading-[0.813rem] font-light text-center sm:text-left">{t("footer.rights")}</p>
-          <p className="text-center sm:text-left"><a
-            href={`/assets/files/${t("footer.privacyPolicyFile")}`}
-            rel="noreferrer"
-            target="_blank"
-            className="font-poppins underline text-[10px] sm:self-end lg:self-auto"
-            aria-label={t("footer.privacyPolicyAriaLabel")}
-          >
-            {t("footer.privacyPolicyText")}
-          </a></p>
-          </div>
-          
-          <div className="flex flex-col space-y-3 sm:space-y-2 items-center sm:items-end text-xs text-center leading-[0.894rem] lg:leading-[0.975rem] tracking-[0.01em]">
-            <div className="flex space-x-3 sm:space-x-3.5 lg:space-x-4">
-              <Icon name="iso9001" className="h-[47.62px] w-[47.62px] sm:h-[52.82px] sm:w-[52.82px] lg:h-[61.5px] lg:w-[61.5px]" />
-              <Icon name="inet" className="h-[47.62px] w-[47.62px] sm:h-[52.82px] sm:w-[52.82px] lg:h-[61.5px] lg:w-[61.5px]" />
-              <Icon name="iram" className="h-[47.62px] w-[47.62px] sm:h-[52.82px] sm:w-[52.82px] lg:h-[61.5px] lg:w-[61.5px]" />
-            </div>
-            <div className="sm:w-fit">
+          <div className="flex justify-center sm:justify-end gap-4 sm:hidden">
+            <Icon name="iram" className="h-[108px] w-[60px] sm:h-[52.82px] sm:w-[52.82px] lg:h-[61.5px] lg:w-[61.5px]" />
+            <Icon name="inet" className="h-[85px] w-[61px] sm:h-[52.82px] sm:w-[52.82px] lg:h-[61.5px] lg:w-[61.5px]" />
+            <div className="w-[72px]">
               <a
                 href={t("footer.certificateUrl")}
                 rel="noreferrer"
                 target="_blank"
-                className="font-poppins underline text-[10px] sm:self-end lg:self-auto"
+                className="font-poppins underline text-[10px] leading-4 tracking-[0.01em] text-white"
                 aria-label={t("footer.certificateAriaLabel")}
               >
                 {t("footer.certificateText")}
               </a>
             </div>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 font-poppins text-center text-white">
+            <p className="text-[10px] leading-[0.813rem] font-light sm:text-left">{t("footer.rights")}</p>
+            <a
+                href={`/assets/files/${t("footer.privacyPolicyFile")}`}
+                rel="noreferrer"
+                target="_blank"
+                className="underline text-[10px] leading-4 tracking-[0.01em]"
+                aria-label={t("footer.privacyPolicyAriaLabel")}
+              >
+              {t("footer.privacyPolicyText")}
+            </a>
+          </div>          
+        </div>
+        <div className="hidden sm:flex sm:flex-col justify-center sm:justify-end gap-4 sm:gap-6">
+          <div className="flex sm:justify-end lg:justify-center sm:gap-4">
+            <Icon name="iram" className="h-[108px] w-[60px]" />
+            <Icon name="inet" className="h-[85px] w-[61px]" />
+          </div>
+          <div className="w-[72px] sm:w-full">
+            <a
+              href={t("footer.certificateUrl")}
+              rel="noreferrer"
+              target="_blank"
+              className="font-poppins underline text-[10px] lg:text-xs leading-4 tracking-[0.01em] text-white"
+              aria-label={t("footer.certificateAriaLabel")}
+            >
+              {t("footer.certificateText")}
+            </a>
           </div>
         </div>
       </div>
