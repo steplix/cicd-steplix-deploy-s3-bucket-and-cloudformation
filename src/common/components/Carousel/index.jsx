a6eed50462291dbf53ca8171608818ea07be300f
Sebastian <slares@steplix.com>
2023-06-21 14:11:42 -0300

    [SW-286] Create abstract carousel, remove duplicates

--- src/common/components/Carousel/index.jsx
+++ src/common/components/Carousel/index.jsx
@@ -15,24 +15,46 @@ const adaptiveHeight = (slider, adaptiveHeightValue) => {
   slider.on('created', updateHeight);
 };
 
-const PortfolioCarousel = ({ children, carouselClassName, spacing, adaptiveHeightValue }) => {
+// carouselClassName: set the responsive height of the cards
+// cardClassName: set the active card attributes. For this carousel to work with the scale effect,
+// an active classname for the cards needs to be created in styles.css, along with an inactive class. See clients section in home for example
+
+const Carousel = ({
+  children,
+  carouselClassName,
+  adaptiveHeightValue,
+  cardClassName,
+  hasArrows,
+}) => {
   const { t } = useTranslation();
-  const [currentSlide, setCurrentSlide] = React.useState(0);
   const [loaded, setLoaded] = React.useState(false);
 
   const [sliderRef, instanceRef] = useKeenSlider(
     {
       loop: true,
-      initial: 0,
+      initial: 1,
+      slides: {
+        origin: 'center',
+        // perView set to 1.01 to force rendering cards to the sides of the active one
+        perView: 1.01,
+      },
       breakpoints: {
         '(min-width: 1024px)': {
-          slides: { perView: 2, spacing },
+          slides: { perView: 2 },
         },
       },
+
       slideChanged(slider) {
-        setCurrentSlide(slider?.track?.details?.rel);
+        slider.slides.map((slide, index) => {
+          if (index === slider.track.details.rel) {
+            slide.children[0].classList.add(cardClassName);
+          } else {
+            slide.children[0].classList.remove(cardClassName);
+          }
+        });
       },
-      created() {
+      created(slider) {
+        if (slider) slider.slides[1].children[0].classList.add(cardClassName);
         setLoaded(true);
       },
     },
@@ -44,33 +66,19 @@ const PortfolioCarousel = ({ children, carouselClassName, spacing, adaptiveHeigh
       <div ref={sliderRef} className={`keen-slider ${carouselClassName}`}>
         {children}
       </div>
-      {loaded && instanceRef.current && (
-        <div className="dots mt-6 sm:hidden">
-          {[...Array(instanceRef?.current?.track?.details?.slides?.length).keys()].map((idx) => {
-            return (
-              <button
-                key={idx}
-                className={'dot' + (currentSlide === idx ? ' active' : '')}
-                onClick={() => {
-                  instanceRef.current?.moveToIdx(idx);
-                }}
-              />
-            );
-          })}
-        </div>
-      )}
-      {loaded && instanceRef.current && (
+
+      {hasArrows && loaded && instanceRef.current && (
         <>
           <button
             aria-label={t('carouselButtonAriaLabel.backward')}
-            className="h-[48px] hidden sm:block w-[48px] absolute left-0 top-[50%] transform translate-y-[-50%] bg-none rounded-full"
+            className="h-[48px] hidden lg:block w-[48px] absolute left-0 top-[50%] transform translate-y-[-50%] bg-none rounded-full"
             onClick={(e) => e.stopPropagation() || instanceRef?.current.prev()}
           >
             <Icon className="h-[48px] w-[48px]" name="back" />
           </button>
           <button
             aria-label={t('carouselButtonAriaLabel.forward')}
-            className="h-[48px] hidden sm:block w-[48px] absolute right-0 top-[50%] transform translate-y-[-50%]"
+            className="h-[48px] hidden lg:block w-[48px] absolute right-0 top-[50%] transform translate-y-[-50%]"
             onClick={(e) => e.stopPropagation() || instanceRef?.current.next()}
           >
             <Icon className="h-[48px] w-[48px]" name="forward" />
@@ -81,11 +89,12 @@ const PortfolioCarousel = ({ children, carouselClassName, spacing, adaptiveHeigh
   );
 };
 
-PortfolioCarousel.propTypes = {
+Carousel.propTypes = {
   children: propTypes.node.isRequired,
   carouselClassName: propTypes.string.isRequired,
-  spacing: propTypes.number,
+  cardClassName: propTypes.string.isRequired,
   adaptiveHeight: propTypes.number,
+  hasArrows: propTypes.bool,
 };
 
-export default PortfolioCarousel;
+export default Carousel;
