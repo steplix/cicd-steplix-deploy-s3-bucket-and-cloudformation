7e7739a630b681f061049d2205f7d946159c9699
Evelyn De León <evelyn@steplix.com>
2023-08-09 17:51:26 -0300

    [SW-313] Show iran and iqnet logos in footer section

--- src/layouts/Footer/Certificates/index.jsx
+++ src/layouts/Footer/Certificates/index.jsx
@@ -1,23 +1,12 @@
-import PropTypes from 'prop-types';
+import Icon from '@/common/components/Icon';
 
-// import Icon from '@/common/components/Icon';
-
-const Certificates = ({ text }) => {
+const Certificates = () => {
   return (
-    <div className="flex sm:flex-col justify-center sm:justify-end gap-4 sm:gap-6">
-      <div className="flex sm:justify-end lg:justify-center gap-4">
-        {/* <Icon className="h-[108px] w-[60px]" name="iram" />
-        <Icon className="h-[85px] w-[61px]" name="inet" /> */}
-      </div>
-      <p className="font-poppins text-[11px] lg:text-xs leading-4 tracking-[0.01em] text-white">
-        {text}
-      </p>
+    <div className="flex justify-center sm:justify-end gap-4">
+      <Icon className="h-[108px] w-[60px]" name="iram" />
+      <Icon className="h-[85px] w-[61px]" name="inet" />
     </div>
   );
-};
-
-Certificates.propTypes = {
-  text: PropTypes.string.isRequired,
 };
 
 export default Certificates;
