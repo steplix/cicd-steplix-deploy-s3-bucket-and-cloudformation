830297187057a461d7a1e7ee6f800e7acda03da9
Ivan Muntian <dmuntian@steplix.com>
2023-09-22 10:37:21 -0300

    [SW-330] Insight tag script has been implemented and changed key prop of testimonials cards to get rid of warnings

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -53,6 +53,30 @@ function MyApp({ Component, pageProps }) {
                     gtag('event', 'conversion', {'send_to': 'AW-10880705347/OdMNCKmky9QDEMPGqcQo'});
                 `}
       </Script>
+      <Script id="linkedIn" strategy="lazyOnload" type="text/javascript">
+        {`_linkedin_partner_id = "4555345";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
+      </Script>
+      <Script id="linkedIn" strategy="lazyOnload" type="text/javascript">
+        {`(function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);`}
+      </Script>
+      <noscript>
+        <img
+          alt=""
+          height="1"
+          src="https://px.ads.linkedin.com/collect/?pid=4555345&fmt=gif"
+          style={{ display: 'none' }}
+          width="1"
+        />
+      </noscript>
       <Layout>
         <AnimatePresence exitBeforeEnter>
           <Component {...pageProps} key={asPath} />
