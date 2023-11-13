c3857b5ddc01e52a919a239505c6772e453b635f
Lautaro <lrozenblum@steplix.com>
2023-11-13 11:35:15 -0300

    [steplix-web-325] created env file

--- update_variables.sh
+++ update_variables.sh
@@ -7,6 +7,8 @@ ROBOTS="$1"
 # Debugging: Print the assigned values
 echo "ROBOTS=$ROBOTS"
 
+# .env: Creating the .env file
+echo ROBOTS=$ROBOTS >> .env
 
 # Update the .env file with the new values
 sed -i "s#^ROBOTS=.*#ROBOTS=$ROBOTS#" .env
