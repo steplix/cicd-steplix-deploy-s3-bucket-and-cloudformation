c6ae0bab878d261c902a4ffb1fe6ba2fecda38f3
Angeles Maza <angie.maza.89@gamil.com>
2024-03-15 10:10:19 -0300

    [SW-351] cñean section and productCard

---
+++ src/common/components/WhatWeDoCard/indext.jsx
@@ -0,0 +1,6 @@
+
+export const WhatWeDoCard = () => {
+  return (
+    <div class="w-full pl-8 pr-4 pt-6 pb-4 bg-white rounded-[20px] shadow flex-col justify-start items-end gap-6 inline-flex" />
+  );
+};
--- src/pages/[locale]/what-we-do/index.jsx
+++ src/pages/[locale]/what-we-do/index.jsx
@@ -2,9 +2,8 @@ import HeadTag from '@/common/components/HeadTag';
 import { useTranslation } from '@/common/lib/i18n';
 import { getPathSlugs } from '@/common/utils/getPathSlugs';
 import Intro from '@/modules/what-we-do/Intro';
-import OurSteps from '@/modules/what-we-do/OurSteps';
 import TransitionWrapper from '@/common/components/TransitionWrapper';
-import CustomNextLink from '@/common/components/CustomNextLink';
+import { WhatWeDoCard } from '@/common/components/WhatWeDoCard/indext';
 
 export default function WhatWeDo({ locale }) {
   const { t } = useTranslation(locale);
@@ -19,27 +18,11 @@ export default function WhatWeDo({ locale }) {
         }}
       />
       <TransitionWrapper className="w-full h-full relative flex flex-col">
-        <div
-          className={`z-[-1] bg-what-we-do-steps sm:bg-what-we-do-steps-sm lg:bg-what-we-do-steps-lg xl:bg-what-we-do-steps-xl bg-center bg-no-repeat absolute w-full ${
-            locale !== 'en' ? 'top-0' : '-top-2'
-          } sm:top-0 bg-origin-content h-[1287px] sm:h-[1145px] lg:h-[1250px] xl:h-[1249px]`}
-        />
         <div className="container section-container w-full mx-auto flex-col min-h-[1276px] sm:min-h-[1144px] lg:min-h-[1250px]">
           <Intro locale={locale} />
-          <OurSteps locale={locale} />
-        </div>
-        <div className="container section-container w-full flex justify-center">
-          <CustomNextLink to={`/${locale}/about-us`}>
-            <h2 className="text-3xl lg:text-5xl font-black font-sofia leading-6 lg:leading-8 tracking-[0.06em] lg:tracking-[0.12em]">
-              #Digital{' '}
-              <span
-                className="text-outlined--purple tracking-[0.08em] lg:tracking-[0.12em]"
-                data-content="Sherpas"
-              >
-                Sherpas
-              </span>
-            </h2>
-          </CustomNextLink>
+          <div className="mt-10">
+            <WhatWeDoCard />
+          </div>
         </div>
       </TransitionWrapper>
     </>
